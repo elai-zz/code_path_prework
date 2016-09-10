a07bc34ce93cd8ccef1e6d9b75afe8dc3dbba9ac
elai <minzeycat@gmail.com>
2016-09-09 22:12:58 -0700

    Initial Commit

---
+++ CodePathTipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodePathTipCalculator
+//
+//  Created by Estella Lai on 9/9/16.
+//  Copyright © 2016 Estella Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
